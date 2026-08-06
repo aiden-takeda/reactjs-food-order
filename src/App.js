be02fc49b50ac759ac9bb375e9c3339d8676f428
Aiden Takeda <aiden.takeda@proton.me>
2026-08-06 15:51:31 +0300

    fix the typo in src/App.js

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
 
   return (
     <CartProvider>
-      {cardIsShown && <Card onClose={hideCartHandler} />}
+      {cardIsShown && <Cart onClose={hideCartHandler} />}
       <Header onShowCart={showCartHandler} />
       <main>
         <Meals />
